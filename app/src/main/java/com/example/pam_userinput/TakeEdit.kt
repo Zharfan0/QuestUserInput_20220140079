6243fe47242a9c1ca6868fd89ace2b3b0322aefe
Zharfan <zharfanwidyadana@gmail.com>
2025-10-30 15:07:51 +0700

    membuat padding Column

--- app/src/main/java/com/example/pam_userinput/TakeEdit.kt
+++ app/src/main/java/com/example/pam_userinput/TakeEdit.kt
@@ -1,9 +1,13 @@
 package com.example.pam_userinput
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -19,4 +23,7 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember {mutableStateOf("")}
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column (modifier = Modifier.padding(top = 50.dp),
+        ){  }
 }
