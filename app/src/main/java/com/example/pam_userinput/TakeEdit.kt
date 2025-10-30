1a3387868e4afc1271fc3b5c48ae20e8d42845da
Zharfan <zharfanwidyadana@gmail.com>
2025-10-30 15:22:18 +0700

    Membuat Row untuk pilihan gender

--- app/src/main/java/com/example/pam_userinput/TakeEdit.kt
+++ app/src/main/java/com/example/pam_userinput/TakeEdit.kt
@@ -2,7 +2,13 @@ package com.example.pam_userinput
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,6 +35,25 @@ fun FormDataDiri(modifier: Modifier
     Column (modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = (Text(text = "Nama Lengkap")),
+            onValueChange = {
+                textNama = it
+            }
+        )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = {textJK = item}
+                ), verticalAlignment = Alignment.CenterVertically){
 
+                }
+            }
+        }
     }
 }
